7da2a6420d3ec714d075f2c60ad3e104a686894f
edilsonnog <edilson.n@hotmail.com>
2021-06-17 19:39:05 -0300

    router

--- src/routes/PrivateRoutes.tsx
+++ src/routes/PrivateRoutes.tsx
@@ -1,20 +1,18 @@
 import React, { useEffect, useState } from 'react'
-import { Redirect, Route, RouteProps } from 'react-router-dom';
+import { Redirect, Route } from 'react-router-dom';
 import { useAuth } from '../context/AuthContext';
 import api from '../services/api';
 
-interface RoutesPropsData extends RouteProps {
+const PrivateRoutes: React.FC<{
     role?: string;
     component: React.FC;
     path: string;
-}
-
-const PrivateRoutes: React.FC<RoutesPropsData> = ({ component, path, role, ...rest }) =>  {
+}> = ({ component, path, role, ...rest }) => {
     const [permissions, setPermissions] = useState([] as string[]);
     useEffect(() => {
         async function loadRoles() {
             const response = api.get('/users/roles');
-            const findRole = (await response).data.some((r: string) => 
+            const findRole = (await response).data.some((r: string) =>
                 role?.split(",").includes(r));
             setPermissions(findRole);
         }
@@ -22,14 +20,14 @@ const PrivateRoutes: React.FC<RoutesPropsData> = ({ component, path, role, ...re
     }, [role])
     const { userLogged } = useAuth();
 
-    if (!userLogged()){
-        return <Redirect to= "/" />;
+    if (!userLogged()) {
+        return <Redirect to="/" />;
     }
 
-    if (!role && userLogged()){
+    if (!role && userLogged()) {
         return <Route {...rest} />;
     }
-    return  permissions ? (<Route path={path} component={component} {...rest} />) : <Redirect to='/' />;
+    return permissions ? (<Route path={path} component={component} {...rest} />) : <Redirect to='/' />;
 }
 
 export default PrivateRoutes;
