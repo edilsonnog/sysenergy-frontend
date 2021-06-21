78970c8c7178af67f2a0b781bb01c8c08706ae6a
edilsonon <edilson.n@hotmail.com>
2021-06-21 16:02:23 -0300

    'router'

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import { HashRouter } from 'react-router-dom';
 
 function App() {
   return (
-    <HashRouter>
+    <HashRouter basename='/'>
       <AuthProvider>
         <Routes />
         <ToastContainer
