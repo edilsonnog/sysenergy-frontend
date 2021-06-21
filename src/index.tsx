f0f3264f74dc9e615dc31a4fe55ca5876280aa0c
edilsonon <edilson.n@hotmail.com>
2021-06-21 15:44:09 -0300

    'router'

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,9 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'antd/dist/antd.css'
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import 'antd/dist/antd.css'
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
   <React.StrictMode>
