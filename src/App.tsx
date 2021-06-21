e145ce9a5ec8d5faa069547ae60fd0ca3761c18e
edilsonon <edilson.n@hotmail.com>
2021-06-21 15:30:55 -0300

    'routas'

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import { AuthProvider } from './context/AuthContext';
 import Routes from './routes';
 import { ToastContainer } from 'react-toastify';
